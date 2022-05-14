74a387dbadc6e49e443096be061f63e371478759
Saaaaakibhai <69164727+Saaaaakibhai@users.noreply.github.com>
2022-05-14 23:55:35 +0600

    MLE

---
+++ Assigment/Untitled1.c
@@ -0,0 +1,64 @@
+/*
+#include<stdio.h>
+int main(void)
+{
+    int m=5, n=2;
+    printf("Enter two integer: ");
+    scanf("%d%d", &m, &n);
+    m = m + 5;
+    n = 3 * n;
+    printf("m = %d\nn = %d\n", m, n);
+    return 0;
+}
+
+
+#define PI 3.14159
+
+void main()
+{
+
+double radius;
+double area;
+double circumf;
+int num_circ ;
+char circ_name;
+
+}
+*/
+/*
+#include<stdio.h>
+int main(void)
+{
+    double x,y,m,n;
+    printf("Enter The Value: ");
+    scanf("%lf",x);
+    scanf("%lf",y);
+    scanf("%lf",m);
+    scanf("%lf",n);
+
+    x / (double)m;
+    x / m;
+    (double)(n * m);
+    (double)(n / m) + y;
+    (double)(n / m);
+
+    return 0;
+}
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int First,Second,Third,Response;
+    printf("Enter First integers: ");
+  scanf("%d", &First);
+    printf("Enter second integers: ");
+  scanf("%d", &Second);
+    printf("Enter Third integers: ");
+  scanf("%d", &Third);
+  Response = First + Second + Third;
+  printf("Sum Of The Number is %d",&Response);
+    return 0;
+}
+
